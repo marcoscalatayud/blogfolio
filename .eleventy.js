ae9eb8824f067dd77aedc245e8420a421387e834
marcos <marccalatayudmallent@gmail.com>
2024-12-30 15:28:09 +0100

    Dates: fix date formatting with luxon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
@@ -18,6 +19,15 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setFreezeReservedData(false);
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addFilter("postDate", date => {
+    if (date && typeof date.getMonth === "function") {
+      return DateTime.fromJSDate(date).toFormat('LLL dd, yyyy')
+    }
+    if (typeof date === "object") {
+      return DateTime.fromObject(date).toFormat('LLL dd, yyyy')
+    }
+    return DateTime.fromISO(date).toFormat('LLL dd, yyyy')
+  })
   return {
     dir: {
       // ⚠️ These values are both relative to your input directory.
